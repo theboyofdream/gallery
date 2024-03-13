df359df0e02d8fa05319bf77c31da27bfbdd7996
theboyofdream <infinityknight024@gmail.com>
2024-03-14 00:35:07 +0530

    bot: auto saving progress. date- 14-03-2024, time- 0:35:07.57

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { FlatList, Image, RefreshControl, SafeAreaView, ScrollView, Text, View } from "react-native";
 import * as fs from 'react-native-fs'
 import Storage from "./storage";
 import { FileLogger } from "react-native-file-logger";
@@ -20,8 +20,23 @@ import { FileLogger } from "react-native-file-logger";
 //   }
 // }
 
-
-type files = { [key: string]: files }
+type file = {
+  name: string,
+  path: string,
+  size: number,
+  ctime: number,
+  mtime: number,
+}
+type files = { [filePath: string]: file }
+type folder = {
+  name: string,
+  path: string,
+  size: number,
+  ctime: number,
+  mtime: number,
+  files: string[]
+}
+type folders = { [folderPath: string]: folder }
 
 
 export default function App() {
@@ -32,7 +47,10 @@ export default function App() {
     }
   }
 
-  const [files, setFile] = useState<files>({})
+  const [folders, setFolders] = useState<folders>({})
+  const [files, setFiles] = useState<files>({})
+  const [scanning, setScanning] = useState(false)
+
   function getParentPath(path: string) {
     let paths = path.split('/')
     paths.pop()
@@ -43,35 +61,69 @@ export default function App() {
     '/storage/emulated/0/Android/data',
   ]
   async function getAllFiles(basePath: string) {
-    let files: files = {}
-    if (excludePaths.includes(basePath)) {
-      return files;
-    }
-    console.log({ basePath })
-    if ((await fs.stat(basePath)).isDirectory()) {
-      let ls = await fs.readdir(basePath)
-      console.log({ ls })
-      for (let name of ls) {
-        let newPath = basePath + '/' + name
-        console.log({ name, newPath })
-        if (await fs.exists(newPath)) {
-          let fileInfo = await fs.stat(newPath)
-          if (fileInfo.isDirectory()) {
-            files[name] = await getAllFiles(newPath)
-          } else {
-            files[name] = {}
+    setScanning(true)
+    let _folders = {} as folders
+    let _files = {} as files
+
+    async function scan(basePath: string) {
+      if (excludePaths.includes(basePath)) {
+        return;
+      }
+      // console.log({ basePath })
+      if ((await fs.stat(basePath)).isDirectory()) {
+        let ls = await fs.readdir(basePath)
+        // console.log({ ls })
+        for (let name of ls) {
+          let newPath = basePath + '/' + name
+          // console.log({ name, newPath })
+          if (await fs.exists(newPath)) {
+            let fileInfo = await fs.stat(newPath)
+            if (!fileInfo.name || fileInfo.name.length < 1) {
+              fileInfo.name = fileInfo.originalFilepath.split('/').pop()
+            }
+            if (fileInfo.isDirectory()) {
+              // if(_folders[name])
+              // fls[name] = await scan(newPath)
+              // console.log(fileInfo.originalFilepath)
+              _folders[newPath] = {
+                name: fileInfo.name ?? '',
+                path: fileInfo.originalFilepath,
+                ctime: fileInfo.ctime,
+                mtime: fileInfo.mtime,
+                size: fileInfo.size,
+                files: []
+              }
+              await scan(newPath)
+            } else {
+              // fls[name] = {}
+              _folders[getParentPath(newPath)].files.push(newPath)
+              _files[newPath] = {
+                name: fileInfo.name ?? '',
+                path: fileInfo.originalFilepath,
+                ctime: fileInfo.ctime,
+                mtime: fileInfo.mtime,
+                size: fileInfo.size
+              }
+            }
           }
         }
       }
+      // return fls;
     }
-    return files;
+
+    await scan(basePath);
+
+    // console.log(_folders)
+    setFolders(_folders)
+    setFiles(_files)
+    setScanning(false)
   }
 
   useEffect(() => {
     handleStoragePermission()
     // console.log('init')
     // FileLogger.info('init')
-    // getAllFiles(getParentPath(fs.DownloadDirectoryPath))
+    getAllFiles(getParentPath(fs.DownloadDirectoryPath))
     // .then(console.log)
     // .catch(console.error)
   }, [])
@@ -79,11 +131,50 @@ export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Text>files tree</Text>
+      {/* <FlatList
+        maxToRenderPerBatch={10}
+        data={Object.keys(files)}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => {
+          let file = files[item]
+          return (
+            <View style={{ padding: 24, paddingVertical: 12 }}>
+              <Image
+                src={'file://' + file.path}
+                width={100}
+                height={100}
+              />
+              <Text>name: {file.name}</Text>
+              <Text>path: {file.path}</Text>
+            </View>
+          )
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={scanning}
+            onRefresh={() => getAllFiles(getParentPath(fs.DownloadDirectoryPath))}
+          />
+        }
+      /> */}
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={scanning}
+              onRefresh={() => getAllFiles(getParentPath(fs.DownloadDirectoryPath))}
+            />
+          }
+        >
+          {
+            Object.values(files).map(f =>
+              <View style={{ padding: 24, paddingVertical: 12 }} key={f.path}>
+                <Text>name: {f.name}</Text>
+                <Text>path: {f.path}</Text>
+              </View>
+            )
+          }
+        </ScrollView>
+      </View>
     </SafeAreaView>
   )
-
-  function Tree() {
-
-
-  }
 }
